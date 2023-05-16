0c0bdd1a0bd8d5433dfbdb72985322b7d419df75
Bluorenge <bluorenge@gmail.com>
2023-05-16 12:07:09 +0300

    remove client ya url

--- packages/client/src/constants/main.ts
+++ packages/client/src/constants/main.ts
@@ -1,14 +1,11 @@
-import { isServer } from 'helpers/window';
-
 const isDev = process.env.NODE_ENV === 'development';
-const ourServer = isDev
+
+const OUR_SERVER = isDev
   ? 'http://localhost:3001'
   : 'https://game-4nly.onrender.com';
 
-export const YANDEX_API_URL = isServer
-  ? `${ourServer}/api/v2` // чтобы запросы шли через мидлвару на нашем сервере
-  : 'https://ya-praktikum.tech/api/v2';
+export const YANDEX_API_URL = `${OUR_SERVER}/api/v2`; // чтобы запросы шли через мидлвару на нашем сервере
 
-export const OUR_API_URL = `${ourServer}/api/v1`;
+export const OUR_API_URL = `${OUR_SERVER}/api/v1`;
 
 export const LEADERBOARD_NAME = 'the-star-wars-kitten';
